e3be3e070df39786c249a1e907855e14f2b8d168
Your Name <you@example.com>
2020-05-24 16:09:41 +0800

    使用yml設定要讀取的人物/社團,支援多個監看

--- money.go
+++ money.go
@@ -6,30 +6,33 @@ import (
 	"io/ioutil"
 	"net/http"
 	"time"
-
-	yamlv2 "gopkg.in/yaml.v2"
+	"sync"
+	"gopkg.in/yaml.v2"
 )
 
 // Request http
 type Request struct {
 	ArtCtn string `json:"ArtCtn"`
+	ArtCteTm string `json:"ArtCteTm"`
+	ChlCap string `json:"ChlCap"`
+	ChlID string `json:"ChlId"`
 }
 
 //yml設定
 var config = &conf{}
-type conf struct {
+type conf []struct {
 	URL     string `yaml:"url"`
+	ChlID string `yaml:"ChlId"`
 }
 
-var data1 = ""
-var count = 0
+var wg sync.WaitGroup
 
 func (c *conf) getConf() *conf {
 	yamlFile, err := ioutil.ReadFile("config.yml")
 	if err != nil {
 		fmt.Println("yamlFile.Get err   #%v ", err)
 	}
-	err = yamlv2.Unmarshal(yamlFile, c)
+	err = yaml.Unmarshal(yamlFile, c)
 	if err != nil {
 		fmt.Println("Unmarshal: %v", err)
 	}
@@ -38,50 +41,66 @@ func (c *conf) getConf() *conf {
 
 func main() {
 	config.getConf()
-	for {
-		run()
-		// err := run()
-		// if err != nil {
-		// 	break
-		// }
-		time.Sleep(500 * time.Millisecond)
-	}
+	data, err := json.Marshal(config)
+
+    if err != nil {
+        fmt.Println("err:\t", err.Error())
+        return
+    }
+
+    c := conf{}
+	err = json.Unmarshal(data,&c)
+	if err != nil {
+		fmt.Println("err:\t", err.Error())
+        return
+ 	}
+	
+	wg.Add(len(c))
+	for i := 0; i < len(c); i++ {
+        go run(c[i].URL,c[i].ChlID)
+    }
+
+	wg.Wait()
+
 }
 
-func run() error {
-	url := config.URL
+func run(url string, chlID string) error {
 	method := "GET"
-
 	client := &http.Client{}
-	req, err := http.NewRequest(method, url, nil)
 
-	if err != nil {
-		fmt.Println(err)
-	}
+	var createTime = ""
+	var count = 0
+	
+	for {
+		req, err := http.NewRequest(method, url, nil)
 
-	res, err := client.Do(req)
-	if err != nil {
-		defer res.Body.Close()
-	}
-
-	body, err := ioutil.ReadAll(res.Body)
-	if err == nil {
-
-		request := []Request{}
-		_ = json.Unmarshal(body, &request)
-		// if err != nil {
-		// 	fmt.Println(err)
-		// 	return err
-		// }
-		
-		if request[0].ArtCtn != data1 {
-			count ++
-			data1 = request[0].ArtCtn
-			fmt.Println(fmt.Sprintf("第%v次PO文",count))
-			fmt.Println(request[0].ArtCtn)
-			fmt.Println("\n")
+		if err != nil {
+			fmt.Println(err)
 		}
 
+		res, err := client.Do(req)
+		if err != nil {
+			defer res.Body.Close()
+		}
+
+		body, err := ioutil.ReadAll(res.Body)
+		if err == nil && len(body) > 0 {
+
+			request := []Request{}
+			_ = json.Unmarshal(body, &request)
+			
+			if request[0].ArtCteTm > createTime && request[0].ChlID == chlID {
+				count ++
+				createTime = request[0].ArtCteTm
+				fmt.Println(request[0].ChlCap,fmt.Sprintf(" 第%v次PO文 ",count),request[0].ArtCteTm)
+				fmt.Println(request[0].ArtCtn)
+				fmt.Println("\n")
+			}
+
+		}
+		time.Sleep(500 * time.Millisecond)
 	}
+
+	
 	return nil
 }
